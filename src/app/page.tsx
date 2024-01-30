b00db2499b019a7f97cfdce85d26575c26f8b9a4
phoenix-flyhigh <j.akshaya07@gmail.com>
2024-01-30 17:39:03 +0530

    Add background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello world
+    <main className="flex items-center gap-6 md:p-6 md:bg-white bg-magnolia md:h-[60%] md:w-[60%] w-screen h-screen relative">
+      <div
+        className="absolute bg-no-repeat bg-left w-[274px] h-full rounded-lg hidden md:block"
+        style={{ backgroundImage: "url('./images/bg-sidebar-desktop.svg')" }}
+      />
+      <div
+        className="absolute md:hidden block bg-no-repeat bg-top w-full h-[172px] top-0"
+        style={{ backgroundImage: "url('./images/bg-sidebar-mobile.svg')" }}
+      />
     </main>
-  )
+  );
 }
