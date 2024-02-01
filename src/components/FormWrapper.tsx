e084ca6db5500afde7ecea39f3ceb40d3c740f7c
phoenix-flyhigh <j.akshaya07@gmail.com>
2024-02-01 11:23:23 +0530

    Fix form wrapper styles

--- src/components/FormWrapper.tsx
+++ src/components/FormWrapper.tsx
@@ -3,7 +3,7 @@ import MultiStepForm from "./MultiStepForm";
 
 const FormWrapper = () => {
   return (
-    <div className="flex md:w-[80%] h-[90%] md:mt-0 mt-24 md:px-12 md:py-6">
+    <div className="flex md:w-[80%] h-[90%] md:mt-0 mt-24 md:px-12">
       <MultiStepForm />
     </div>
   );
